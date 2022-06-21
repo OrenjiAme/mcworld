ac83c04c30236089dbec23662e02771315d62bb6
orenjiame <motoya6160@gmail.com>
2022-06-21 13:30:53 +0900

    a

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
 mc.regPlayerCmd("hello","say hello!",function(pl,args){
     pl.tell("Hello!");
 });
+
+mc.regConsoleCmd("oumu","引数の言葉をオウム返しします",oncmd_oumu)
+
+function oncmd_oumu(pl,args){
+    if(args.length == 1){
+        pl.tell(args[0])
+    }else{
+        pl.tell("引数を指定してください")
+    }
+}
+
+mc.listen("onJoin",join())
+
+function join(){
+    mc.broadcast("こんにちは");
+}
